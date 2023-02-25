0371cd7481ae5afd4c0e8ad09bdd44d03336a486
Abdullah Khetran <khetranabdullah@gmail.com>
2023-02-25 22:47:30 +0500

    added code for step00

---
+++ step00_helloworld/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+
+
+export default function Page() {
+  return (
+    <h1 className='text-3xl font-bold underline'>Hello World</h1>
+  )
+}
